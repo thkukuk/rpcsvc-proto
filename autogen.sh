a1aee2431c391e566b8eeda328ad43a0b2e061be
Thorsten Kukuk <kukuk@suse.com>
2022-01-07 14:27:10 +0100

    Update gettext artifacts

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,10 @@
 #!/bin/sh -x
 
 rm -rfv config.sub config.guess config.h.in aclocal.m4 config.rpath m4
+autopoint --force
+rm -fv po/Makevars.template po/ChangeLog m4/ChangeLog
 aclocal
 autoheader
-#libtoolize --automake --copy
 automake --add-missing --copy --force
 autoreconf --install
 chmod 755 configure
